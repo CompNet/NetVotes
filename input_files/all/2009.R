667d6f7331af274f7c8010cd2fdbf357cb3e378e
israelzinc <israel.zinc@gmail.com>
2015-09-17 15:09:03 +0200

    Added module to save community vector

---
+++ input_files/all/2009.R
@@ -0,0 +1,12 @@
+MP.political_groups <- c("all")
+MP.countries        <- c("all")
+MP.mp_ids           <- c("all")
+MP.group_by         <- c("DontGroup")
+MP.group_by.mode    <- c("DontGroup")
+DOCS.time_limits    <- c("01/01/2009","30/12/2009")
+DOCS.policies       <- c("all")
+selected.table      <- c("2")
+
+alpha <- 0.3
+
+file.title         <- c("all_2009")
