c05102b8eadb088c9362607f1903933c940eb6fa
Vincent Labatut <vince.labatut@gmail.com>
2018-02-27 14:53:51 +0100

    updated plot methods: layout based on signed laplacian

--- src/plot-tools/plot-networks.R
+++ src/plot-tools/plot-networks.R
@@ -4,6 +4,82 @@
 # 11/2015 Vincent Labatut
 #############################################################################################
 library("igraph")
+library("expm")
+
+
+
+#############################################################################################
+# This layout for signed graphs is based on some generalization of the the Laplacian for 
+# signed graph. Several ones were proposed in the following articles:
+#	- Spectral Analysis of Signed Graphs for Clustering, Prediction and Visualization
+#		Kunegis, J.; Schmidt, S.; Lommatzsch, A.; Lerner, J.; de Luca, E. & Albayrak, S.
+#		SIAM International Conference on Data Mining, 2010, p559-570.
+#	- Spectral embedding of signed networks
+#		Zheng, Q. & Skillicorn, D. B.
+#		SIAM International Conference on Data Mining, 2015, p55-63.
+#
+# g: graph to process, it must possess a weight edge parameter containing the signed weights
+#    of the links. It is supposed to be undirected: if it is directed, we first colapse the 
+#	 links before processing it.
+# method: which generalization of the signed Laplacian to use:
+#			- "kunegis": Signed Laplacian (Kunegis et al. 2010)
+#			- "rw_kunegis": Random walk normalized signed Laplacian (Kunegis et al. 2010)
+#			- "sn_kunegis": Symmetric normalized signed Laplacian (Kunegis et al. 2010)
+#			- "sn_zheng": Simple normalized signed Laplacian (Zheng & Skillicorn 2015) 
+#			- "bn_zheng": Balanced normalized signed Laplacian (Zheng & Skillicorn 2015) 
+#############################################################################################
+layout.signed.laplacian <- function(g, method="bn_zheng")
+{	# possibly make the graph undirected
+	if(is.directed(g))
+	{	warning("The graph should be undirected: we collapse its links before going on")
+		g <- as.undirected(graph=g, mode="collapse", edge.attr.comb="mean")
+	}
+	
+	# weighted adjacency matrix
+	W <- - as.matrix(get.adjacency(graph=g, type="both", attr="weight")) # TODO don't know why we need to take the opposite of the matrix. there's a sign mistake somewhere, but couldn't find it.
+	# positive weighted adjacency matrix
+	Wp <- W
+	Wp[which(Wp<0)] <- 0
+	# negative weighted adjacency matrix
+	Wm <- -W
+	Wm[which(Wm<0)] <- 0
+	
+	# positive strength matrix
+	Sp <- matrix(0,nrow(W),ncol(W))
+	diag(Sp) <- apply(Wp, 1, sum)
+	# negative strength matrix
+	Sm <- matrix(0,nrow(W),ncol(W))
+	diag(Sm) <- apply(Wm, 1, sum)
+	# total strength matrix
+	S <- Sp + Sm
+	
+	# process the laplacian
+	if(method=="kunegis")
+		# signed Laplacian
+		L <- S - W
+	else if(method=="rw_kunegis")
+		# random walk normalized signed Laplacian
+		L <- diag(ncol(W)) - solve(S) %*% W
+	else if(method=="sn_kunegis")
+		# symmetric normalized signed laplacian
+		L <- diag(ncol(W)) - S %^% (-1/2) %*% W %*% S %^% (-1/2)
+	else if(method=="sn_zheng")
+		# simple normalized signed Laplacian
+		L <- solve(S) %*% (Sp - W)
+	else if(method=="bn_zheng")
+		# balanced normalized signed Laplacian 
+		L <- solve(S) %*% (Sp - Sm - W)
+	else
+		stop("Could not recognize the specified method '",method,"'")
+	
+	# take its first two Eigenvectors
+	ev <- eigen(L, symmetric=TRUE)$vec
+	lay <- ev[,1:2]
+#	lay <- t(ev[1:2,])
+	print(lay)
+	
+	return(lay)
+}
 
 
 
@@ -13,10 +89,11 @@ library("igraph")
 # g: signed graph to plot.
 # plot.file: base name (and path) of the generated files.
 # format: format(s) to handle (among "PDF", "PNG", and NA for screen).
+# method: layout used (signed graphs).
 #
 # returns: the same graph, with the spatial positions stored as nodal attributes x and y.
 #############################################################################################
-plot.network <- function(g, membership=NA, plot.file, format=c("PDF","PNG",NA))
+plot.network <- function(g, membership=NA, plot.file, format=c("PDF","PNG",NA), method="bn_zheng")
 {	# setup node parameters
 	vertex.sizes <- 10
 	vertex.label <- V(g)$MEPid
@@ -40,10 +117,13 @@ plot.network <- function(g, membership=NA, plot.file, format=c("PDF","PNG",NA))
 	# setup layout (only if the graph doesn't already have one)
 	v.att <- list.vertex.attributes(graph=g)
 	if(!("x" %in% v.att) | !("y" %in% v.att)) 
-	{	gpos <- delete.edges(graph=g,edges=which(E(g)$weight<0))
-#		lyt <- layout.kamada.kawai(graph=gpos)
-		lyt <- layout.fruchterman.reingold(graph=gpos)
-#		lyt <- layout.circle(graph=gpos)
+	{	# layouts for unsigned graphs
+		gpos <- delete.edges(graph=g,edges=which(E(g)$weight<0))
+##		lyt <- layout.kamada.kawai(graph=gpos)
+#		lyt <- layout.fruchterman.reingold(graph=gpos)
+##		lyt <- layout.circle(graph=gpos)
+		# layouts for signed graphs
+		lyt <- layout.signed.laplacian(g, method)
 		# store spatial positions as nodal attributes
 		V(g)$x <- lyt[,1]
 		V(g)$y <- lyt[,2]
@@ -90,3 +170,13 @@ plot.network <- function(g, membership=NA, plot.file, format=c("PDF","PNG",NA))
 	
 	return(g)
 }
+
+
+
+
+#############################################################################################
+### Test using the article toy graph
+#g2 <-graph.empty(6,directed=FALSE)
+#g2 <- add_edges(g2,edges=c(1,2,2,4,2,6,3,4,4,6,5,6))
+#E(g2)$weight <- c(1,-1,-1,1,-1,1)
+#plot.network(g2, membership=membership, plot.file="sqdq", format=NA, method="bn_zheng")
